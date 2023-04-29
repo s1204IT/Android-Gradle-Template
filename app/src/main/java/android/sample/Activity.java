922cbcc5e841cd2fdf4a73c5e7713247f6cf60b3
Syuugo <pub@s1204.me>
2023-04-29 16:14:32 +0900

    Test no activity

--- app/src/main/java/android/sample/Activity.java
+++
@@ -1,9 +0,0 @@
-package android.sample;
-
-public class Activity extends android.app.Activity {
-    @Override
-    protected void onCreate(android.os.Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        finishAndRemoveTask();
-    }
-}
